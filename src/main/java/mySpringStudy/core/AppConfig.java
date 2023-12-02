b23b2c9edce1cad7afbacbff7d3774fe1bb0f1ac
MJ <rlaalswls102@naver.com>
2023-12-02 17:29:22 +0900

    fix: AppConfig 리펙토링

--- src/main/java/mySpringStudy/core/AppConfig.java
+++ src/main/java/mySpringStudy/core/AppConfig.java
@@ -1,6 +1,8 @@
 package mySpringStudy.core;
 
+import mySpringStudy.core.discount.DiscountPolicy;
 import mySpringStudy.core.discount.FixDiscountPolicy;
+import mySpringStudy.core.member.MemberRepository;
 import mySpringStudy.core.member.MemberService;
 import mySpringStudy.core.member.MemberServiceImpl;
 import mySpringStudy.core.member.MemoryMemberRepository;
@@ -9,11 +11,19 @@ import mySpringStudy.core.order.OrderServiceImpl;
 
 public class AppConfig {
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private static MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
     }
 
 }
