81373cd41f5390d0de026c9403d89001f6774bff
MJ <rlaalswls102@naver.com>
2023-12-04 21:27:00 +0900

    feat: 다양한 설정 형식 지원 - java, xml

---
+++ src/test/java/mySpringStudy/core/xml/XmlAppContext.java
@@ -0,0 +1,19 @@
+package mySpringStudy.core.xml;
+
+import mySpringStudy.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.test.context.support.GenericXmlContextLoader;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class XmlAppContext {
+    
+    @Test
+    void xml_테스트() throws Exception {
+        GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
