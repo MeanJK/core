b21e7e14d9126f795ad32f5d72ef4fbc2c2483d2
MJ <rlaalswls102@naver.com>
2023-12-23 16:58:29 +0900

    feat: scope 와 provider

--- src/main/java/mySpringStudy/core/web/LogDemoService.java
+++ src/main/java/mySpringStudy/core/web/LogDemoService.java
@@ -3,14 +3,16 @@ package mySpringStudy.core.web;
 
 import lombok.RequiredArgsConstructor;
 import mySpringStudy.core.common.MyLogger;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
 
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
     public void logic(String testId) {
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.log("service id = " + testId);
     }
 }
