50b426211a36e08b4ede91efe9a2a02edb5212cb
MJ <rlaalswls102@naver.com>
2023-12-23 17:26:23 +0900

    feat: scope 와 프록시 사용

--- src/main/java/mySpringStudy/core/web/LogDemoService.java
+++ src/main/java/mySpringStudy/core/web/LogDemoService.java
@@ -3,16 +3,14 @@ package mySpringStudy.core.web;
 
 import lombok.RequiredArgsConstructor;
 import mySpringStudy.core.common.MyLogger;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
 
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
     public void logic(String testId) {
-        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.log("service id = " + testId);
     }
 }
