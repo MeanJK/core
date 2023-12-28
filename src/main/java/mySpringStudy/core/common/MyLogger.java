81f73ea02c76be0a9d71f184a4d60fdf3fef1085
MJ <122874405+MeanJK@users.noreply.github.com>
2023-12-28 23:49:27 +0900

    Update MyLogger.java

--- src/main/java/mySpringStudy/core/common/MyLogger.java
+++ src/main/java/mySpringStudy/core/common/MyLogger.java
@@ -13,21 +13,17 @@ import java.util.UUID;
 public class MyLogger {
     private String uuid;
     private String requestUrl;
-
     public void setRequestUrl(String requestUrl) {
         this.requestUrl = requestUrl;
     }
-
     public void log(String message){
         System.out.println("[" + uuid + "]" + "[" + requestUrl + "]" + message);
     }
-
     @PostConstruct
     public void init(){
         uuid = UUID.randomUUID().toString();
         System.out.println("[" + uuid + "] request scope bean create: " + this);
     }
-
     @PreDestroy
     public void close(){
         System.out.println("[" + uuid + "] request scope bean close: " + this);
